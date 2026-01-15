8ae8d7567eb69434f5657c9e1240c2d52e6c0523
Guy Ron <guy.ron2@mail.huji.ac.il>
2026-01-15 14:38:01 +0200

    Fix deployment: Ensure adapter is installed Thu Jan 15 14:38:01 EET 2026

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   adapter: netlify(),
   i18n: {
     defaultLocale: "en",
--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'dummy-token', queries,  });
+export const client = createClient({ cacheDir: '/Users/ronguy/Dropbox/Work/Yael/WebSite/tina/__generated__/.cache/1768480635600', url: 'https://content.tinajs.io/2.0/content/dummy-client-id/github/main', token: 'dummy-token', queries,  });
 export default client;
   
