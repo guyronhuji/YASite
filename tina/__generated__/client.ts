46c67aeb6ba1472d3b7fcf767a7c574bba5c205c
Guy Ron <guy.ron2@mail.huji.ac.il>
2026-01-29 15:02:04 +0200

    Fix deployment: Ensure adapter is installed Thu Jan 29 15:02:04 IST 2026

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'dummy-token', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'd4eb5eee9f56a92b0e027074ab84ab3e447cab0e', queries,  });
 export default client;
   
